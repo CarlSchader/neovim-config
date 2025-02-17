784c8bacd1bc59e4b2960e3d68411eaf058a61d4
Carl Schader <carl.schader@saronic.com>
2025-02-16 20:34:25 -0600

    rm auto install

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,61 +13,63 @@ return {
     end,
   },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "typescript",
-        "python",
-        "rust",
-        "swift",
-        "javascript",
-      },
-    },
-  },
 
-  -- Copilot stuff
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    cmd = "Copilot",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-        },
-        panel = { enabled = false },
-      })
-    end,
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = {
+  --     ensure_installed = {
+  --       "vim",
+  --       "lua",
+  --       "vimdoc",
+  --       "html",
+  --       "css",
+  --       "typescript",
+  --       "python",
+  --       "rust",
+  --       "swift",
+  --       "javascript",
+  --     },
+  --   },
+  -- },
 
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-          require("copilot_cmp").setup()
-        end,
-      },
-    },
-    opts = {
-      sources = {
-        { name = "nvim_lsp", group_index = 2 },
-        -- { name = "copilot",  group_index = 2 },
-        { name = "luasnip",  group_index = 2 },
-        { name = "buffer",   group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path",     group_index = 2 },
-      },
-    },
-  },
+  -- -- Copilot stuff
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   event = "InsertEnter",
+  --   cmd = "Copilot",
+  --   config = function()
+  --     require("copilot").setup({
+  --       suggestion = {
+  --         enabled = true,
+  --         auto_trigger = true,
+  --       },
+  --       panel = { enabled = false },
+  --     })
+  --   end,
+  -- },
+  --
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     {
+  --       "zbirenbaum/copilot-cmp",
+  --       config = function()
+  --         require("copilot_cmp").setup()
+  --       end,
+  --     },
+  --   },
+  --   opts = {
+  --     sources = {
+  --       { name = "nvim_lsp", group_index = 2 },
+  --       -- { name = "copilot",  group_index = 2 },
+  --       { name = "luasnip",  group_index = 2 },
+  --       { name = "buffer",   group_index = 2 },
+  --       { name = "nvim_lua", group_index = 2 },
+  --       { name = "path",     group_index = 2 },
+  --     },
+  --   },
+  -- },
+
 
   -- -- Avante
   -- {
