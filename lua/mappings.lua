40b91bc91a67c1b52b4a8f63263ca74af5836f28
Carl Schader <carl.schader@saronic.com>
2025-05-07 23:06:53 -0500

    cant for the life of me get a toggle theme mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,7 +7,3 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>", { desc = "quick <ESC> in normal mode" })
 
 map("n", "gr", function() vim.lsp.buf.references() end, { desc = "LSP go to references" })
-
--- map("n", "<A-t>", function()  end, { desc = "Toggle theme" })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
