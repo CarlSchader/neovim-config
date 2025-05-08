ad72ef3b1e8956ca9d82a73cb7267a51ea8bfcb9
Carl Schader <carl.schader@saronic.com>
2025-05-07 22:07:37 -0500

    mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,13 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
 map("i", "jk", "<ESC>", { desc = "quick <ESC> in normal mode" })
+
 map("n", "gr", function() vim.lsp.buf.references() end, { desc = "LSP go to references" })
+
+-- map("n", "<A-t>", function()  end, { desc = "Toggle theme" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
