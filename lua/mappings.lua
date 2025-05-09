24605f4fb0d9a1f3278bc28ba56fb7df1df71064
Carl Schader <carl.schader@saronic.com>
2025-05-09 04:06:13 -0500

    correct mapping help

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
-map("i", "jk", "<ESC>", { desc = "quick <ESC> in normal mode" })
+map("i", "jk", "<ESC>", { desc = "quick <ESC> in insert mode" })
 
 map("n", "gr", function() vim.lsp.buf.references() end, { desc = "LSP go to references" })
