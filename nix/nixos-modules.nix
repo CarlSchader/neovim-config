7bc94973467c09886582183bca45cc7882961bb2
Carl Schader <carl.schader@saronic.com>
2025-12-13 06:08:57 -0700

    fix-pkgs

--- nix/nixos-modules.nix
+++ nix/nixos-modules.nix
@@ -1,10 +1,10 @@
-{ self, pkgs, ... }:
+{ self, ... }:
 {
   target-path ? "/etc/xdg/nvim",
 }:
 {
   nixosModules.default =
-    { ... }:
+    { pkgs, ... }:
     {
       environment.etc.nvim = {
         source = self.${pkgs.system}.packages.default;
