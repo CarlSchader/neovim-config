bba5b1a93a0ef3d17ebc79ae911329026dce4e61
Carl Schader <carl.schader@saronic.com>
2025-12-13 05:27:34 -0700

    lint

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
         inherit (nixpkgs) lib;
+        pkgs = import nixpkgs { inherit system; };
         src = lib.fileset.toSource {
           root = ./.;
           fileset = ./.;
         };
       in
       {
-        packages.default = pkgs.runCommand "nvim-config" {} ''
+        packages.default = pkgs.runCommand "nvim-config" { } ''
           cp -r ${src} $out
         '';
       }
