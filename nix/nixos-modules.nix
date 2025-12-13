6145b0cc26881093b557b87f19f2226141813e53
Carl Schader <carl.schader@saronic.com>
2025-12-13 06:29:51 -0700

    simpler-module

--- nix/nixos-modules.nix
+++ nix/nixos-modules.nix
@@ -1,21 +1,10 @@
 { self, ... }:
 {
   nixosModules.default =
-    { pkgs, lib, config, ... }:
+    { pkgs, ... }:
     {
-      options.programs.neovim-config = {
-        enable = lib.mkEnableOption "neovim-config";
-        targetPath = lib.mkOption {
-          type = lib.types.str;
-          default = "xdg/nvim";
-          description = "Target path relative to /etc/ for the neovim config";
-        };
-      };
-
-      config = lib.mkIf config.programs.neovim-config.enable {
-        environment.etc.${config.programs.neovim-config.targetPath} = {
-          source = self.packages.${pkgs.system}.default;
-        };
+      environment.etc."xdg/nvim" = {
+        source = self.packages.${pkgs.system}.default;
       };
     };
 }
