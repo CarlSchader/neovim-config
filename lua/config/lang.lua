c4630d90ad7174a2e67393f833bc16e5b7cf77fe
carlschader <carlschader@proton.me>
2026-04-05 19:24:15 -0400

    fix: ruff_linter -> ruff_format

--- lua/config/lang.lua
+++ lua/config/lang.lua
@@ -28,7 +28,7 @@ end
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "ruff_lint" },
+		python = { "ruff_format" },
 		nix = { "nixfmt" },
 		rust = { "rustfmt" },
 		c = { "clang-format" },
