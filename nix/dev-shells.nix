6b69daf97f92b2412599864a1a277f3b204af764
carlschader <carlschader@proton.me>
2026-04-05 12:14:38 -0400

    ran formatting

--- nix/dev-shells.nix
+++ nix/dev-shells.nix
@@ -1,11 +1,13 @@
 { nixpkgs, flake-utils, ... }@inputs:
-flake-utils.lib.eachDefaultSystem (system:
-let
-	pkgs = import nixpkgs { inherit system; };
-	inherit (import ./common.nix { inherit pkgs; }) buildInputs;
-in
-{
-	devShells.default = pkgs.mkShell {
-		inherit buildInputs;
-	};
-})
+flake-utils.lib.eachDefaultSystem (
+  system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+    inherit (import ./common.nix { inherit pkgs; }) buildInputs;
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      inherit buildInputs;
+    };
+  }
+)
--- nix/nixos-modules.nix
+++ nix/nixos-modules.nix
@@ -1,34 +1,36 @@
 { ... }:
 {
-	nixosModules.default = { pkgs, ... }:
-	let
-		inherit (import ./common.nix { inherit pkgs; }) buildInputs;
-	in
-	  {
-		environment.systemPackages = buildInputs;
+  nixosModules.default =
+    { pkgs, ... }:
+    let
+      inherit (import ./common.nix { inherit pkgs; }) buildInputs;
+    in
+    {
+      environment.systemPackages = buildInputs;
 
-		environment.etc."xdg/nvim" = {
-		  source = ./..;
-		};
-	  };
-
-	homeModules.default = { pkgs, lib, ... }:
-	let
-	inherit (import ./common.nix { inherit pkgs; }) buildInputs;
-  in
-  {
-    home.packages = buildInputs;
-
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
-
-    xdg.configFile."nvim" = {
-      source = lib.cleanSourceWith {
-        src = ./..;
-        filter = path: type: !(lib.hasPrefix "lazy-lock.json" path);
+      environment.etc."xdg/nvim" = {
+        source = ./..;
       };
-      recursive = true;
     };
-  };
+
+  homeModules.default =
+    { pkgs, lib, ... }:
+    let
+      inherit (import ./common.nix { inherit pkgs; }) buildInputs;
+    in
+    {
+      home.packages = buildInputs;
+
+      home.sessionVariables = {
+        EDITOR = "nvim";
+      };
+
+      xdg.configFile."nvim" = {
+        source = lib.cleanSourceWith {
+          src = ./..;
+          filter = path: type: !(lib.hasPrefix "lazy-lock.json" path);
+        };
+        recursive = true;
+      };
+    };
 }
