49fa371e33aa688958740abb2650668ffc9e7409
carlschader-acs <carl.schader@allencontrolsystems.com>
2026-05-01 00:49:48 -0500

    single package works

--- nix/packages.nix
+++ nix/packages.nix
@@ -2,16 +2,90 @@
 flake-utils.lib.eachDefaultSystem (
   system:
   let
-    inherit (nixpkgs) lib;
     pkgs = import nixpkgs { inherit system; };
-    src = lib.fileset.toSource {
-      root = ../.;
-      fileset = ../.;
+
+    extraPackages = import ./extra-packages.nix { inherit pkgs; };
+
+    treesitterWithParsers = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+      p.c
+      p.cpp
+      p.lua
+      p.rust
+      p.python
+      p.nix
+      p.bash
+      p.markdown
+      p.markdown_inline
+    ]);
+
+    plugins = with pkgs.vimPlugins; [
+      # theme
+      vim-moonfly-colors
+
+      # ui
+      mini-nvim
+      bufferline-nvim
+      nvim-web-devicons
+      bufdelete-nvim
+      lualine-nvim
+      indent-blankline-nvim
+      which-key-nvim
+      neo-tree-nvim
+      nui-nvim
+
+      # git
+      lazygit-nvim
+      gitlinker-nvim
+      gitsigns-nvim
+      git-blame-nvim
+      plenary-nvim
+
+      # telescope
+      telescope-nvim
+      telescope-fzf-native-nvim
+
+      # treesitter
+      treesitterWithParsers
+
+      # lsp + completion
+      nvim-lspconfig
+      conform-nvim
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp-nvim-lua
+      luasnip
+      cmp_luasnip
+
+      # copilot
+      copilot-lua
+      copilot-cmp
+    ];
+
+    neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+      plugins = map (plugin: { inherit plugin; }) plugins;
+      extraLuaPackages = ps: [ ps.jsregexp ];
+      withPython3 = false;
+      withNodeJs = false;
+      withRuby = false;
     };
   in
   {
-    packages.default = pkgs.runCommand "nvim-config" { } ''
-      cp -r ${src} $out
-    '';
+    packages.default = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
+      neovimConfig
+      // {
+        wrapRc = false;
+        wrapperArgs = neovimConfig.wrapperArgs ++ [
+          "--set"
+          "NIX_NEOVIM"
+          "1"
+          "--suffix"
+          "PATH"
+          ":"
+          "${pkgs.lib.makeBinPath extraPackages}"
+        ];
+      }
+    );
   }
 )
