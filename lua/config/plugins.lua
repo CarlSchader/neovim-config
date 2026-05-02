aee9d71ac502a2cb527ae93b4641d164bbff76ce
Carl Schader <carlschader@proton.me>
2026-05-02 12:01:54 -0500

    File tree tracks buffer

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -16,6 +16,17 @@ require("bufferline").setup({
 	},
 })
 
+-- neo-tree
+require("neo-tree").setup({
+	filesystem = {
+		follow_current_file = {
+			enabled = true,
+			leave_dirs_open = false,
+		},
+		use_libuv_file_watcher = true,
+	},
+})
+
 -- gitlinker
 require("gitlinker").setup({
 	opts = {
@@ -80,8 +91,7 @@ local cmp = require("cmp")
 local has_words_before = function()
 	unpack = unpack or table.unpack
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0
-		and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
--- scripts/simlink-nvim.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-ROOT_DIR="$(git rev-parse --show-toplevel)"
-
-ln -s "$ROOT_DIR" "$HOME/.config/nvim-dev"
-
-export NVIM_APPNAME="nvim-dev"
