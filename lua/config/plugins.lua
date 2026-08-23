abe30d8594f62bd5c92e55d56741727fed837d14
Carl Schader <carlschader@proton.me>
2026-08-23 02:32:53 -0500

    fix: tab correction on cmp

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -113,24 +113,15 @@ cmp.setup({
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
-				cmp.select_next_item()
-			elseif vim.snippet.active({ direction = 1 }) then
-				vim.schedule(function()
-					vim.snippet.jump(1)
-				end)
-			elseif has_words_before() then
-				cmp.complete()
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
+
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif vim.snippet.active({ direction = -1 }) then
-				vim.schedule(function()
-					vim.snippet.jump(-1)
-				end)
+				cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
 			else
 				fallback()
 			end
